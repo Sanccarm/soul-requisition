9a8080e05fde09c790ca8136ccf88924c595250d
Carmelo S. <sanccarm@mail.gvsu.edu>
2025-11-18 02:10:56 -0500

    "parry working. Will add stuff in another commit"

--- scripts/soul.gd
+++ scripts/soul.gd
@@ -1,6 +1,8 @@
 extends Area2D
 signal soul_collected
 
+@onready var collapse_sound: AudioStreamPlayer = $Collapse
+
 func _ready() -> void:
 	$AnimatedSprite2D.play()
 	# Connect the body_entered signal for collision detection
@@ -12,14 +14,17 @@ func _on_body_entered(body: Node) -> void:
 	zoomout.parallel().tween_property($"../Player/Camera2D", "zoom:x", 1.0, 1.5)
 	zoomout.parallel().tween_property($"../Player/Camera2D", "zoom:y", 1.0, 1.5)
 
+	
 	# Check if the colliding body is the player
 	if body.is_in_group("Player"):
+		if collapse_sound:
+			collapse_sound.play()
 		# Emit signal to notify that soul was collected
 		soul_collected.emit()
 		# Hide the soul item
 		visible = false
 		# Disable collision detection
 		set_process(false)
-		# Remove the soul item after a short delay
-		await get_tree().create_timer(0.1).timeout
-		queue_free()
+	# Remove the soul item after a short delay
+	await get_tree().create_timer(0.1).timeout
+	queue_free()
