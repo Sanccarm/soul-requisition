20ee2c143f58e278defd17d1d509407b98bbdd55
Carmelo S. <sanccarm@mail.gvsu.edu>
2025-11-19 22:38:04 -0500

    added more logic to main menu and a button!!!

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,18 +1,26 @@
 extends Control
 @onready var title = $ColorRect/title
+@onready var start = $ColorRect/VBoxContainer/StartButton
 
 
 func _ready():
 	await get_tree().create_timer(2).timeout
-	move_title_up_and_play_music()
+	await move_title_up_and_play_music()
+	button_fade_in()
 
-	
-	
-	pass
 
 
 func move_title_up_and_play_music():
 	var move_title = create_tween()
-	move_title.tween_property(title, "position:y", 50, 1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	move_title.tween_property(title, "position:y", 278.0, 1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
 	await move_title.finished
 	$MenuMusic.play()
+	emit_signal("finished")
+	
+func button_fade_in():
+	var starttween = create_tween()
+	starttween.tween_property(start, "modulate:a", 1.0, 1.0)
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
